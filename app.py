e3dcbd8edde4d931e79c590e792c5b94540246cc
Victor Vasiliev <victor@imperson.com>
2019-11-27 18:33:39 +0200

    Syntactic optimization.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from requests import HTTPError
 from DialogFlowManager import processor
 from DialogFlowManager.custom_exceptions import DialogFlowLoadComponentError
 
-# Consts.
-
 
 # Init app.
 app = Flask(__name__)
@@ -27,9 +25,11 @@ def handle_bad_request(err):
 def exchange(component_id, session_id):
     request_json = request.get_json() or {}
 
+    user_input = request_json.get("user_input")
+
     response = processor.process_request(component_id=component_id,
                                          session_id=session_id,
-                                         text=request_json.get("user_input"))
+                                         text=user_input)
 
     return jsonify({"response": response.query_result.fulfillment_text}), 200, {}
 
